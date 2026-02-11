170f661ac977d9346da9aea7c66a9e40330773a2
Jeriel Beckford <jerielweb@outlook.com>
2026-02-10 23:40:24 -0600

    gg

--- server/config/server.ts
+++ server/config/server.ts
@@ -4,4 +4,9 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+    transfer: {
+        remote: {
+          enabled: env.bool('STRAPI_TRANSFER_REMOTE_ENABLED', true),
+    },
+  },
 });
