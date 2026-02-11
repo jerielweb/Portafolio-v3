9aa936b8da8edae4ff44d56c2ba1cd4b8511ca74
Jeriel Beckford <jerielweb@outlook.com>
2026-02-10 21:15:52 -0600

    Load Container

---
+++ server/dist/src/api/technology-list/services/technology-list.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * technology-list service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::technology-list.technology-list');
